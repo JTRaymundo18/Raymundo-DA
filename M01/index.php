9ad0a8174c18c5cdaa81cbb821e1df7233aca78e
JTRaymundo18 <justineraymundo18@gmail.com>
2024-11-11 20:19:47 +0800

    Add files via upload

--- M01/index.php
+++ M01/index.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['email'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php echo $_SESSION['firstName'] . " " . $_SESSION['lastName'] ?></title>
+  <link rel="icon" href="images/usericon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
--- M01/signup.php
+++ M01/signup.php
@@ -32,6 +32,7 @@ $result = executeQuery($query);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>M01</title>
+  <link rel="icon" href="images/usericon.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="shared/css/style.css">
